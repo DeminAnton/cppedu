e30c934c3438b2f1448c97f7689bdcf25fe5d5f1
DeminAnton <demantser@gmail.com>
2024-07-31 13:35:39 +0800

    add ex3

---
+++ 3/example1.cpp
@@ -0,0 +1,15 @@
+// counter!!!!
+
+#include <iostream>
+
+int main(){
+    int count = 0;
+
+    while (count <= 10){
+        std::cout << count << std::endl;
+        count++;
+    }
+    
+    
+    return 0;
+}
